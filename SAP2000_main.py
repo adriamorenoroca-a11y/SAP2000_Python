0c4adea3477680a481524f56671b61d074f8635b
adriamorenoroca-a11y <adria.moreno.roca@estudiantat.upc.edu>
2026-05-11 20:08:28 +0200

    Lectura de materiales, secciones y armaduras

--- SAP2000_main.py
+++ SAP2000_main.py
@@ -1,6 +1,6 @@
 from SAP2000_conexion    import conectar_sap
 from SAP2000_excel       import leer_excel
-
+from SAP2000_materiales  import crear_materiales, crear_secciones, crear_armaduras
 
 
 def generar_modelo(ruta_excel):
@@ -10,6 +10,11 @@ def generar_modelo(ruta_excel):
 
     # ── 2. Lectura Excel ──────────────────────────────────────────────
     datos = leer_excel(ruta_excel)
+
+    # ── 3. Materiales + Secciones + Armaduras ─────────────────────────
+    crear_materiales(SapModel, datos)
+    crear_secciones(SapModel, datos)
+    crear_armaduras(SapModel, datos)
 
 if __name__ == "__main__":
     generar_modelo(r"C:\Users\adria.moreno\OneDrive - Global Infrastructure\Python - SAP2000\Coordenadas_Nodos.xlsx")
