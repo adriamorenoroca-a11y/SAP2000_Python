dc039fee6466d6ea896fae295363d84b715e68fe
adriamorenoroca-a11y <adria.moreno.roca@estudiantat.upc.edu>
2026-05-11 20:07:31 +0200

    Lectura de excel

--- SAP2000_main.py
+++ SAP2000_main.py
@@ -1,5 +1,5 @@
 from SAP2000_conexion    import conectar_sap
-
+from SAP2000_excel       import leer_excel
 
 
 
@@ -8,7 +8,8 @@ def generar_modelo(ruta_excel):
     # ── 1. Conexión SAP2000 ───────────────────────────────────────────
     SapModel = conectar_sap()
 
-
+    # ── 2. Lectura Excel ──────────────────────────────────────────────
+    datos = leer_excel(ruta_excel)
 
 if __name__ == "__main__":
     generar_modelo(r"C:\Users\adria.moreno\OneDrive - Global Infrastructure\Python - SAP2000\Coordenadas_Nodos.xlsx")
