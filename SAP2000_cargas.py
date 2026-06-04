ac8ea54fe4618075075b910ca8bf58c8185ed3aa
adriamorenoroca-a11y <adria.moreno.roca@estudiantat.upc.edu>
2026-06-04 23:05:27 +0200

    Corrección de cargas en areas nuevas

--- SAP2000_cargas.py
+++ SAP2000_cargas.py
@@ -115,4 +115,10 @@ def aplicar_cargas(SapModel, datos, areas_modulos, areas_nuevas_nombres, ejes):
         for area_data in areas_modulo:
             aplicar_a_area(area_data[1])
 
+    # Areas nuevas de la abertura
+    if opening=="Yes:":
+        for area_name in areas_nuevas_nombres:
+            aplicar_a_area(area_name)
+        print("Cargas aplicadas a areas nuevas de la abertura")
+
     print("Cargas aplicadas correctamente")
